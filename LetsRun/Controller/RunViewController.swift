da99d7112ed904dd357f33b5982559bef72a7ca9
Rehan Parkar <parkarcamp@gmail.com>
2018-05-08 21:28:35 -0500

    added poly line rendering on the map

--- LetsRun/Controller/RunViewController.swift
+++ LetsRun/Controller/RunViewController.swift
@@ -25,22 +25,49 @@ class RunViewController: LocationViewController {
         
         checkLocationAuthStatus()
         
-        mapView.delegate = self
+
         
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    func getLastRun() {
-        guard let lastRun = Run.getAllRuns()?.first else {
-            closeLastRunView()
-            return}
+    func addLastRunToMap() -> MKPolyline? {
         
-        showLastRunView()
+        guard let lastRun = Run.getAllRuns()?.first else {return nil}
         avgPaceLabel.text = lastRun.pace.formatTimeDurationToString()
         distanceLabel.text = "\(lastRun.distance.metersToMiles(places: 2)) mi"
         durationLabel.text = lastRun.duration.formatTimeDurationToString()
         
+        var coordinates = [CLLocationCoordinate2D]()
+        for location in lastRun.locations {
+            coordinates.append(CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude))
+        }
+        return MKPolyline(coordinates: coordinates, count: lastRun.locations.count)
     }
+    
+    func setUpView() {
+        if let overLay = addLastRunToMap() {
+            
+            if mapView.overlays.count > 0 {
+                mapView.removeOverlays(mapView.overlays)
+            }
+            mapView.add(overLay)
+            showLastRunView()
+        } else {
+            closeLastRunView()
+        }
+    }
+    
+//    func getLastRun() {
+//        guard let lastRun = Run.getAllRuns()?.first else {
+//            closeLastRunView()
+//            return}
+//
+//        showLastRunView()
+//        avgPaceLabel.text = lastRun.pace.formatTimeDurationToString()
+//        distanceLabel.text = "\(lastRun.distance.metersToMiles(places: 2)) mi"
+//        durationLabel.text = lastRun.duration.formatTimeDurationToString()
+//
+//    }
     
     func closeLastRunView() {
         
@@ -55,11 +82,17 @@ class RunViewController: LocationViewController {
         lastRunStack.isHidden = false
         closeViewButton.isHidden = false
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        setUpView()
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
        
         manager?.delegate = self //as? CLLocationManagerDelegate
+        mapView.delegate = self
         manager?.startUpdatingLocation()
-        getLastRun()
+       // getLastRun()
     }
     
     override func viewDidDisappear(_ animated: Bool) {
@@ -88,5 +121,15 @@ extension RunViewController: CLLocationManagerDelegate {
             mapView.showsUserLocation = true
             mapView.userTrackingMode = .follow
         }
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let polyLine = overlay as! MKPolyline
+        let renderer = MKPolylineRenderer (polyline: polyLine)
+        renderer.strokeColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+        renderer.lineWidth = 5
+        
+        return renderer
+        
     }
 }
